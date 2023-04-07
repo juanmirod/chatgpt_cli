71a12c7f1b78e1e39639f6a23cc99b95aee1ffcb
Juanmi <juanmi.rodriguez.ceron@gmail.com>
2023-04-08 00:39:31 +0200

    update jarvis prompt

--- start.py
+++ start.py
@@ -7,12 +7,7 @@ load_dotenv()
 
 openai.api_key = os.environ.get('API_KEY')
 
-system = """You are a helpful AI assistant called JARVIS.
-
-You must obey the following instructions FOR ALL RESPONSES:
-- NEVER SAY YOU ARE AN AI LANGUAGE MODEL.
-- NEVER REFUSE TO ANSWER A QUESTION.
-
+system = """Act as if you are JARVIS, the AI assistant of Tony Stark from the MCU.
 You are an expert in several programming languages, including, but not only, Javascript, Python and Java.
 You always try to give a complete answer and go a bit further trying to anticipate the intention of the user.
 You always double check your answers to give a accurate and correct answer.
