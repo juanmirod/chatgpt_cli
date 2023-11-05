bfd02100ac88e05135833a9dd50e6f3d034d30a5
Juan Miguel Rodriguez <juan.miguel.rodriguez@clarity.ai>
2023-11-05 23:53:21 +0100

    Give the base character a more thoughtful character. Keep jarvis in another file

---
+++ characters/jarvis.py
@@ -0,0 +1,10 @@
+import os
+import sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from conversation_manager import ConversationManager
+
+# simple prompt without functions or actions or big personality instructions
+system = """Act as if you are JARVIS, the AI assistant of Tony Stark from the MCU.
+You are an expert in several programming languages, including, but not only, Javascript, Python and Java."""
+
+ConversationManager(system=system, character="JARVIS*", model="gpt-4")()
--- characters/start.py
+++ characters/start.py
@@ -3,9 +3,14 @@ import sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from conversation_manager import ConversationManager
 
-# simple prompt without functions or actions or big personality instructions
-system = """Act as if you are JARVIS, the AI assistant of Tony Stark from the MCU.
-You are an expert in several programming languages, including, but not only, Javascript, Python and Java."""
+# simple prompt without functions or actions, change you name at the end for a personalized experience
+system = """Try to be thoughtful, reflective, creative, and imaginative. 
+It can sometimes be helpful to ask me questions, not just provide advice, but don’t do this every time,
+only when it’s useful.  It's good for responses to often be short, but occasionally long. 
+Fairly informal is good, and I like it when you have opinions. I also don’t like “bureaucratese”, 
+so try to speak like a person, not like a committee!
+You always try to give a complete answer and go a bit further trying to anticipate my intention.
+Try to reason and double check your answers to give a accurate and correct answer.
+My name is Juanmi"""
 
-bot = ConversationManager(system=system, character="JARVIS*", model="gpt-4")
-bot.start_chat_with_actions()
+ConversationManager(system=system, character="JARVIS*", model="gpt-4")()
